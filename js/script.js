7c95436b21c350b8820771a803c00110028f0d55
Sergey2212 <serg-kot@yandex.ru>
2021-01-12 05:05:26 +0400

    30 Действия со страницей

--- js/script.js
+++ js/script.js
@@ -1,18 +1,29 @@
-/* Задания на урок:
+//  Задания на урок:
 
-1) Удалить все рекламные блоки со страницы (правая часть сайта)
+// 1) Удалить все рекламные блоки со страницы (правая часть сайта)
+ let adv = document.querySelectorAll('.promo__adv img');
+// for (let i = 0; i < adv.length; i++) {
+//     adv[i].remove();
+// }
+adv.forEach(element => {
+    element.remove();
+});
 
-2) Изменить жанр фильма, поменять "комедия" на "драма"
+//2) Изменить жанр фильма, поменять "комедия" на "драма"
+let promoGenre = document.querySelector('.promo__genre');
+//promoGenre.innerHTML = 'Драма';
+promoGenre.textContent = 'Драма';
 
-3) Изменить задний фон постера с фильмом на изображение "bg.jpg". Оно лежит в папке img.
-Реализовать только при помощи JS
+// 3) Изменить задний фон постера с фильмом на изображение "bg.jpg". Оно лежит в папке img.
+// Реализовать только при помощи JS
+let promoBg = document.querySelector('.promo__bg');
+//promoBg.style.background = 'url(img/bg.jpg)';
+promoBg.style.backgroundImage = 'url("img/bg.jpg")';
 
-4) Список фильмов на странице сформировать на основании данных из этого JS файла.
-Отсортировать их по алфавиту 
+// 4) Список фильмов на странице сформировать на основании данных из этого JS файла.
+// Отсортировать их по алфавиту 
 
-5) Добавить нумерацию выведенных фильмов */
-
-'use strict';
+//5) Добавить нумерацию выведенных фильмов 
 
 const movieDB = {
     movies: [
@@ -24,3 +35,12 @@ const movieDB = {
     ]
 };
 
+let arrMovi = movieDB.movies.sort();
+const filmList = document.querySelector('.promo__interactive-list');
+filmList.innerHTML = "";
+arrMovi.forEach((book, i) => {
+    filmList.innerHTML += `<li class="promo__interactive-item">
+                                ${i+1}.  ${book}
+                                <div class="delete"></div>
+                            </li>`;
+});
